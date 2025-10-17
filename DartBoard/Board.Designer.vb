7c5aee22a56ce8d63304adb94d35904804da84d8
Payden Hoskins <paydenhoskins@gmail.com>
2025-10-17 12:37:34 -0600

    Added code

--- DartBoard/Board.Designer.vb
+++ DartBoard/Board.Designer.vb
@@ -24,7 +24,22 @@ Partial Class Board
     Private Sub InitializeComponent()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
         Me.ExitButton = New System.Windows.Forms.Button()
+        Me.ThrowButton = New System.Windows.Forms.Button()
+        Me.ReviewButton = New System.Windows.Forms.Button()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.GameButton = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.DartDisplayButton = New System.Windows.Forms.Button()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'PictureBox1
@@ -32,38 +47,179 @@ Partial Class Board
         Me.PictureBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
             Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.PictureBox1.Location = New System.Drawing.Point(6, 4)
+        Me.PictureBox1.BackColor = System.Drawing.Color.DarkGray
+        Me.PictureBox1.Location = New System.Drawing.Point(6, 6)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(789, 338)
+        Me.PictureBox1.Size = New System.Drawing.Size(802, 328)
         Me.PictureBox1.TabIndex = 0
         Me.PictureBox1.TabStop = False
         '
         'ExitButton
         '
+        Me.ExitButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.ExitButton.BackColor = System.Drawing.Color.Red
         Me.ExitButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ExitButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        Me.ExitButton.Location = New System.Drawing.Point(644, 362)
+        Me.ExitButton.Location = New System.Drawing.Point(664, 351)
         Me.ExitButton.Name = "ExitButton"
         Me.ExitButton.Size = New System.Drawing.Size(144, 76)
         Me.ExitButton.TabIndex = 1
         Me.ExitButton.Text = "&Exit"
         Me.ExitButton.UseVisualStyleBackColor = False
         '
+        'ThrowButton
+        '
+        Me.ThrowButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.ThrowButton.BackColor = System.Drawing.Color.ForestGreen
+        Me.ThrowButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ThrowButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.ThrowButton.Location = New System.Drawing.Point(6, 351)
+        Me.ThrowButton.Name = "ThrowButton"
+        Me.ThrowButton.Size = New System.Drawing.Size(147, 76)
+        Me.ThrowButton.TabIndex = 2
+        Me.ThrowButton.Text = "Throw"
+        Me.ThrowButton.UseVisualStyleBackColor = False
+        '
+        'ReviewButton
+        '
+        Me.ReviewButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ReviewButton.BackColor = System.Drawing.Color.Yellow
+        Me.ReviewButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ReviewButton.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ReviewButton.Location = New System.Drawing.Point(512, 352)
+        Me.ReviewButton.Name = "ReviewButton"
+        Me.ReviewButton.Size = New System.Drawing.Size(146, 75)
+        Me.ReviewButton.TabIndex = 3
+        Me.ReviewButton.Text = "Review"
+        Me.ReviewButton.UseVisualStyleBackColor = False
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Location = New System.Drawing.Point(6, 4)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(822, 471)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.PictureBox1)
+        Me.TabPage1.Controls.Add(Me.ThrowButton)
+        Me.TabPage1.Controls.Add(Me.ReviewButton)
+        Me.TabPage1.Controls.Add(Me.ExitButton)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(814, 445)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "TabPage1"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DartDisplayButton)
+        Me.TabPage2.Controls.Add(Me.ComboBox1)
+        Me.TabPage2.Controls.Add(Me.GameButton)
+        Me.TabPage2.Controls.Add(Me.PictureBox2)
+        Me.TabPage2.Controls.Add(Me.ListBox1)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(814, 445)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "TabPage2"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PictureBox2.BackColor = System.Drawing.Color.DarkGray
+        Me.PictureBox2.Location = New System.Drawing.Point(6, 6)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(802, 328)
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(536, 344)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(277, 95)
+        Me.ListBox1.TabIndex = 1
+        '
+        'GameButton
+        '
+        Me.GameButton.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.GameButton.BackColor = System.Drawing.Color.ForestGreen
+        Me.GameButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GameButton.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.GameButton.Location = New System.Drawing.Point(6, 357)
+        Me.GameButton.Name = "GameButton"
+        Me.GameButton.Size = New System.Drawing.Size(147, 76)
+        Me.GameButton.TabIndex = 3
+        Me.GameButton.Text = "Game"
+        Me.GameButton.UseVisualStyleBackColor = False
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(6, -3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 29)
+        Me.Button1.TabIndex = 5
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(399, 344)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(119, 21)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'DartDisplayButton
+        '
+        Me.DartDisplayButton.Location = New System.Drawing.Point(168, 357)
+        Me.DartDisplayButton.Name = "DartDisplayButton"
+        Me.DartDisplayButton.Size = New System.Drawing.Size(139, 76)
+        Me.DartDisplayButton.TabIndex = 5
+        Me.DartDisplayButton.Text = "Display"
+        Me.DartDisplayButton.UseVisualStyleBackColor = True
+        '
         'Board
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.ExitButton)
-        Me.Controls.Add(Me.PictureBox1)
+        Me.ClientSize = New System.Drawing.Size(833, 477)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TabControl1)
         Me.Name = "Board"
         Me.Text = "Form1"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage2.ResumeLayout(False)
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
 
     Friend WithEvents PictureBox1 As PictureBox
     Friend WithEvents ExitButton As Button
+    Friend WithEvents ThrowButton As Button
+    Friend WithEvents ReviewButton As Button
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents GameButton As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DartDisplayButton As Button
+    Friend WithEvents ComboBox1 As ComboBox
 End Class
--- DartBoard/Board.vb
+++ DartBoard/Board.vb
@@ -1,5 +1,121 @@
-﻿Public Class Board
+﻿Imports System.Linq.Expressions
+
+Public Class Board
+    Public filePath As String = "DartScoreAndRound.txt"
+    Public roundNumber As Integer
+    Public reviewY As Integer
+    Public reviewX As Integer
+    'Event Handlers
+    Private Sub ThrowButton_Click(sender As Object, e As EventArgs) Handles ThrowButton.Click
+
+        Dim DartCount As Integer
+        Do
+            PictureBox1.Refresh()
+            roundNumber += 1
+            Dim X As Integer
+            Dim Y As Integer
+            WriteLine(1, "Round" & CStr(roundNumber))
+            WriteLine(1)
+            'Thow three darts per round
+            For i = 1 To 3
+                X = RandomNumber(PictureBox1.Width)
+                Y = RandomNumber(PictureBox1.Height)
+                DartCount += 1
+                WriteLine(1, $"Dart {DartCount}")
+                WriteLine(1, "X Coordinate: " & CStr(X) & " Y Coordinate: " & CStr(Y))
+                WriteLine(1)
+                DrawDart(X, Y)
+                ComboBox1.Items.Add("Round" & CStr(roundNumber) & ":" & " " & "x" & X & "," & "y" & Y)
+            Next
+            DartCount = 0
+            'Prompt to throw again
+        Loop Until MsgBox("3 Darts Thrown, would you like to throw again?", MsgBoxStyle.YesNo) = MsgBoxResult.No
+        'Prompt to clear board
+        If MsgBox("would you like to clear the board?", MsgBoxStyle.YesNo) = MsgBoxResult.Yes Then
+            PictureBox1.Refresh()
+            Defaults()
+        End If
+    End Sub
+
+    Private Sub Board_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Defaults()
+        FileOpen(1, filePath, OpenMode.Append)
+    End Sub
+
+    'Functions and Subroutines
+    Function RandomNumber(max%) As Integer
+        Randomize()
+        Return CInt(System.Math.Floor(CDbl(Rnd() * max))) + 1
+    End Function
+
     Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        WriteLine(1, "----Game Ended----")
+        FileClose(1)
         Me.Close()
+    End Sub
+
+    Sub DrawDart(x%, y%)
+        Dim g As Graphics = PictureBox1.CreateGraphics
+        Dim pen As New Pen(Color.Red)
+        Dim dartWidth% = 30
+
+        g.DrawEllipse(pen, x - (dartWidth \ 2), y - (dartWidth \ 2), dartWidth, dartWidth)
+        g.DrawLine(pen, x - 3, y, x + 3, y)
+        g.DrawLine(pen, x, y - 3, x, y + 3)
+
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub DrawDart2(x%, y%)
+        Dim g As Graphics = PictureBox2.CreateGraphics
+        Dim pen As New Pen(Color.Red)
+        Dim dartWidth% = 30
+
+        g.DrawEllipse(pen, x - (dartWidth \ 2), y - (dartWidth \ 2), dartWidth, dartWidth)
+        g.DrawLine(pen, x - 3, y, x + 3, y)
+        g.DrawLine(pen, x, y - 3, x, y + 3)
+
+        pen.Dispose()
+        g.Dispose()
+    End Sub
+
+    Sub Defaults()
+        PictureBox1.BackColor = Color.DarkGray
+    End Sub
+
+    Private Sub ReviewButton_Click(sender As Object, e As EventArgs) Handles ReviewButton.Click
+        Dim currentRecord As String = ""
+        TabControl1.SelectedTab = TabPage2
+        'Read and display the log file to listbox
+        ListBox1.Items.Clear()
+        FileClose(1)
+        FileOpen(2, filePath, OpenMode.Input)
+        Do Until EOF(2)
+            Input(2, currentRecord)
+            Dim data As String = currentRecord
+            ListBox1.Items.Add(data)
+        Loop
+    End Sub
+
+    Private Sub GameButton_Click(sender As Object, e As EventArgs) Handles GameButton.Click
+        FileClose(2)
+        TabControl1.SelectedTab = TabPage1
+        ListBox1.Items.Clear()
+        Defaults()
+        FileOpen(1, filePath, OpenMode.Append)
+    End Sub
+
+    Private Sub DartDisplayButton_Click(sender As Object, e As EventArgs) Handles DartDisplayButton.Click
+        Dim displayX As Integer
+        Dim displayY As Integer
+        Dim coordnates As String
+        coordnates = ComboBox1.SelectedItem.ToString()
+        coordnates = Replace(coordnates, "Round " & "x" & "y" & ":", "")
+        Dim coords() As String
+        coords = Split(coordnates, ", ")
+        displayX = CInt(coords(0))
+        displayX = CInt(coords(1))
+        DrawDart2(displayX, displayY)
     End Sub
 End Class
